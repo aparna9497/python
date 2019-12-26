2ccfce4b9cf25eb199ac6b2b097e29dc11636d28
Aparna <aparnaswaroopasree88@gmail.com>
2019-12-26 17:10:52 +0530

    sine cosine added

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,13 @@
 print('hello World')
 print(str(10//3))
+print(str(10//3))
+print('*' * 10)
+price1 = 10
+price2 = 20
+print(price1 * price2) 
+name = input('What is your Name ')
+print('Hi' + name)
+purpose = input('What is your purpose of visit ')
+print('u are allowed if your are here to '+ purpose)
+
+
---
+++ sine_cosine.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0,4*np.pi,0.1)
+y = np.sin(x)
+z = np.cos(x)
+
+plt.plot(x,y,x,z)
+plt.title('Plot of sin and cos from 0 to 4pi')
+plt.legend(['sin(x)', 'cos(x)'])
+plt.xlabel('x values from 0 to 4pi')
+plt.ylabel('sin(x) and cos(x)')
+plt.show()
